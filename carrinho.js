3d65a560c7b609e81c6319516b0d29b8dca65aa8
vhhonorato <vhhonorato@hotmail.com>
2026-06-11 22:04:51 -0300

    feat: Implementada logica para validação de produto com quantidade zerada ou preço negativo. fix: corrigida a referenciação do arquivo produtos,json utilizando o modulo path.join e __dirname, criando um caminho absoluto para o arquivo. fix: alterado do método forEach para for devido a necessidade de usar o 'break'.

--- carrinho.js
+++ carrinho.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+const path = require('path');
 
 
 let carrinho = [];
@@ -8,29 +8,50 @@ let valorTotalComDesconto;
 
 
 try {
-    const data = fs.readFileSync('./dados/produtos.json', 'utf-8'); // Lê o conteúdo do arquivo JSON de forma síncrona.
+    const caminhoArquivo = path.join(__dirname, 'produtos.json');
+    const data = fs.readFileSync(caminhoArquivo, 'utf-8'); // Lê o conteúdo do arquivo JSON de forma síncrona.
     carrinho = JSON.parse(data); // Converte o texto em array/objeto
 
     let valorTotal = 0;
+    let deuErro = false;
 
-    carrinho.forEach(item => {
+    for(const item of carrinho){
         let valorPorIten = item.quantidade * item.precoUnitario;
+        if(item.quantidade <= 0){
+            console.log(`A quantidade do item ${item.produto} não pode ser negativa.`)
+            deuErro = true;
+            break;
+        }else if(item.precoUnitario < 0){
+            console.log(`O preço do item ${item.produto} não pode ser negativo.`)
+            deuErro = true;
+            break;
+        }
         valorTotal += valorPorIten;
     
-    });
-    if(valorTotal >= 100){
+    };
+    if(!deuErro){
+        if(valorTotal >= 100){
         valorTotalComDesconto = valorTotal-(valorTotal * 0.10);
         console.log("DESCONTO DESBLOQUEADO! --- 10% DE DESCONTO APLICADO");
         console.log(`Valor total da compra: R$${valorTotal.toFixed(2)}`)
         console.log(`Valor do desconto: R$${(valorTotal * 0.10).toFixed(2)}`)
         console.log(`Valor total com desconto: R$${valorTotalComDesconto.toFixed(2)}`)
-    }else{
+        }else{
         console.log(`Valor total da compra: R$${valorTotal.toFixed(2)}`)
+        }if(valorTotal >= 100){
+        valorTotalComDesconto = valorTotal-(valorTotal * 0.10);
+        console.log("DESCONTO DESBLOQUEADO! --- 10% DE DESCONTO APLICADO");
+        console.log(`Valor total da compra: R$${valorTotal.toFixed(2)}`)
+        console.log(`Valor do desconto: R$${(valorTotal * 0.10).toFixed(2)}`)
+        console.log(`Valor total com desconto: R$${valorTotalComDesconto.toFixed(2)}`)
+        }else{
+        console.log(`Valor total da compra: R$${valorTotal.toFixed(2)}`)
+        }
     }
+    
 
 
 } catch(error) {
     console.error('Erro ao ler o arquivo JSON:', error);
     console.log("Erro ao ler arquivo produtos:", error.message);
 }
-
