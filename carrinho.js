e2bf342b7a0dff29017f675b743ae97018661932
vhhonorato <vhhonorato@hotmail.com>
2026-06-11 21:13:22 -0300

    fix: reformulado código para usar dados da  lista em um arquivo JSON no diretório 'dados', sem a necessidade de adicionar item por item.

--- carrinho.js
+++ carrinho.js
@@ -1,57 +1,36 @@
-const prompt = require("prompt-sync")({sigint: true}); 
+const fs = require('fs');
+
+
 
 let carrinho = [];
 let valorTotal = 0;
 let valorTotalComDesconto;
 
 
-while (true) {
-   let nomeProduto;
-   let quantidadeProduto;
-   let valorProduto;
+try {
+    const data = fs.readFileSync('./dados/produtos.json', 'utf-8'); // Lê o conteúdo do arquivo JSON de forma síncrona.
+    carrinho = JSON.parse(data); // Converte o texto em array/objeto
 
-    nomeProduto = prompt("Digite o nome do produto, 'total' para finalizar a compra e 'sair' para encerrar: ");
-   if (nomeProduto.toLowerCase() === "sair") {
-      break;
-   }else if(nomeProduto.toLowerCase() === "total"){
-           
-            for (let i = 0; i < carrinho.length; i++) {
-                valorTotal += carrinho[i].quantidade * carrinho[i].valor;   
-            }
-            
-            if(valorTotal >= 100){
-                valorTotalComDesconto = valorTotal-(valorTotal * 0.10);
-                console.log("Para compras com valor acima de R$ 100,00 você terá um desconto de 10%")
-                console.log(`Valor total da compra: R$${valorTotal.toFixed(2)}`)
-                console.log(`Valor total da compra com desconto: R$${valorTotalComDesconto.toFixed(2)}`)
-            }else{
-                console.log(`Valor total da compra: R$${valorTotal.toFixed(2)}`)
-            
-            }    
-        break;
-   }else{
-          
-        if (!isNaN(nomeProduto)&& nomeProduto.trim() !== ""){ //Valida se o nome do produto é apenas um número, o que seria inválido. E verifica se o usário digitou apenas espaços vazios.
-            console.log("Digite um produto válido");
-            nomeProduto = prompt("Digite o nome do produto, 'total' para finalizar a compra e 'sair' para encerrar: ");
-            
-            
-        } 
-        quantidadeProduto = parseInt(prompt(`Digite a quantidade de ${nomeProduto}: `)); // 'parseInt' converte o numéro inserido como strig em número inteiro. 
-        valorProduto = parseFloat(prompt(`Digite o valor em reais de ${nomeProduto}: `)); // 'parseFloat'converte o númeor inserido como string em número com casas decimais.
-   }
+    let valorTotal = 0;
 
-   
+    carrinho.forEach(item => {
+        let valorPorIten = item.quantidade * item.precoUnitario;
+        valorTotal += valorPorIten;
+    
+    });
+    if(valorTotal >= 100){
+        valorTotalComDesconto = valorTotal-(valorTotal * 0.10);
+        console.log("DESCONTO DESBLOQUEADO! --- 10% DE DESCONTO APLICADO");
+        console.log(`Valor total da compra: R$${valorTotal.toFixed(2)}`)
+        console.log(`Valor do desconto: R$${(valorTotal * 0.10).toFixed(2)}`)
+        console.log(`Valor total com desconto: R$${valorTotalComDesconto.toFixed(2)}`)
+    }else{
+        console.log(`Valor total da compra: R$${valorTotal.toFixed(2)}`)
+    }
 
 
-   carrinho.push({
-      nome: nomeProduto,
-      quantidade: quantidadeProduto,
-      valor: valorProduto
-
-   });
-   
-
-   console.log(carrinho);
+} catch(error) {
+    console.error('Erro ao ler o arquivo JSON:', error);
+    console.log("Erro ao ler arquivo produtos:", error.message);
 }
-console.log()
+
